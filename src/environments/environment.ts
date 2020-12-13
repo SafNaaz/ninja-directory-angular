092631e9f28ea32718e4fd092adf64bab238e7d6
Safnas <iamsafnas@gmail.com>
2020-12-14 00:16:18 +0530

    removing keys

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,7 @@
 export const environment = {
   production: false,
   firebase : {
-    apiKey: "AIzaSyD5QQ_8Dphz90BcQ6pdsRg7psWnPUxloV4",
-    authDomain: "nn-angular-basic-a213f.firebaseapp.com",
-    databaseURL: "https://nn-angular-basic-a213f-default-rtdb.firebaseio.com",
-    projectId: "nn-angular-basic-a213f",
-    storageBucket: "nn-angular-basic-a213f.appspot.com",
-    messagingSenderId: "204562191162",
-    appId: "1:204562191162:web:c676a7376195e84267f603"
+    //use yours
   }
 }
 /*
