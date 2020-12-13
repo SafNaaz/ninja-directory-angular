4249202356fdf8049d9b8e687502178f0b8a1c03
Safnas <iamsafnas@gmail.com>
2020-12-13 18:20:07 +0530

    added http module and logged json oninit

---
+++ src/app/data.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(private http: HttpClient) { }
+
+  fetchData(){
+    return this.http.get('assets/ninjas.json').subscribe(
+      (data) => console.log(data)
+    )
+  }
+}
--- src/app/directory/directory.component.ts
+++ src/app/directory/directory.component.ts
@@ -1,28 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { LoggingService } from '../logging.service';
+import { DataService} from '../data.service'
 
 @Component({
   selector: 'app-directory',
   templateUrl: './directory.component.html',
-  styleUrls: ['./directory.component.css']
+  styleUrls: ['./directory.component.css'],
+  providers : [DataService]
 })
 export class DirectoryComponent implements OnInit {
 
   term: any
 
-  ninjas = [
-    {name: "Yoshi", belt : "black"},
-    {name: "Ryu", belt : "red"},
-    {name: "Crystal", belt : "purple"},
-  ]
+  ninjas = []
 
-  constructor(private logger: LoggingService) { }
+  constructor(private logger: LoggingService, private dataService: DataService) { }
 
   logIt(){
     this.logger.log()
   }
 
   ngOnInit(): void {
+    this.dataService.fetchData()
   }
 
 }
